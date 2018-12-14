dd298abe97dc0757ec6bdffa6f0df2893bc06dfc
Stone White <bys_1123@163.com>
2018-12-14 14:21:02 +0800

    Fix Wechat share in topics

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -104,7 +104,8 @@
     $(document).ready(function()
     {
         var $config = {
-            title               : '{{{ $topic->title }}} | from LC #laravel-china# {{ $topic->user->id != 1 ? '@summer_charlie' : '' }} {{ $topic->user->weibo_name ? '@'.$topic->user->weibo_name : '' }}',
+            url                 : location.href,
+            title               : '{{{ $topic->title }}} | from Dimianzhan社区 {{ $topic->user->id != 1 ? '@斯东Stone' : '' }} {{ $topic->user->weibo_name ? '@'.$topic->user->weibo_name : '' }}',
             wechatQrcodeTitle   : "微信扫一扫：分享", // 微信二维码提示文字
             wechatQrcodeHelper  : '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>',
             image               : "{{ $cover ? $cover->link : 'https://dn-phphub.qbox.me/uploads/images/201701/29/1/pQimFCe1r5.png' }}",
